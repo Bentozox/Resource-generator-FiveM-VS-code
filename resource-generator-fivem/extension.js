88d898f14d5e7bf80e6ccb20fb556b255e005be6
Bentozox <60925857+Bentozox@users.noreply.github.com>
2021-08-23 12:18:39 +0200

    Removing useless import

--- resource-generator-fivem/extension.js
+++ resource-generator-fivem/extension.js
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const { readFileSync, readdirSync, mkdirSync, writeFileSync } = require('fs');
-const path = require('path')
 const vscode = require('vscode');
 var extensionPath = "";
 
